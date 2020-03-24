962ea5d85660e6992222b8afd38c225e6e0f558b
John Ghatas <johnghatas97@hotmail.com>
2020-03-25 00:50:10 +0100

    Fixed sonar script

--- scan-project.sh
+++ scan-project.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mvn sonar:sonar \
   -Dsonar.projectKey=open-mrs \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=6c24ea7af66d015db43347b9fed476dbce48efa1
+  -Dsonar.host.url=http://localhost:9000/ \
+  -Dsonar.login=admin \
+  -Dsonar.password=admin
