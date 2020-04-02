bd7cfb6fe30bcc7723943d2be962b2949b0ab257
ammar <ammar_alhashemi@hotmail.com>
2020-04-02 04:58:33 +0200

    tests

--- api/src/test/java/org/openmrs/asvTest.java
+++ api/src/test/java/org/openmrs/asvTest.java
@@ -26,193 +26,170 @@ import org.junit.Before;
  */
 public class asvTest extends BaseContextSensitiveTest {
 
+	/**
+	 * Active Patient's State Test
+	 */
+	/**
+	 * inActive Patient's State Test
+	 */
+    //Arrange
 	//patient's date info
-	private Date leftRange;
-	private Date inRange;
-	private Date rightRange;
-	private Date rightOutOfRange;
-	private Date leftOutOfRange;
+	private Date start;
+	private Date now;
+	private Date end;
+	private Date exceeded;
 
 	@Before
 	public void before() {
-		inRange = new Date();
-		leftRange = new Date(inRange.getTime() - 10000);
-		rightRange = new Date(inRange.getTime() + 10000);
-		rightOutOfRange = new Date(rightRange.getTime() + 10000);
-		leftOutOfRange = new Date(leftRange.getTime() - 10000);
+		now = new Date();
+		start = new Date(now.getTime() - 3121212);
+		end = new Date(now.getTime() + 3121212);
+		exceeded = new Date(end.getTime() + 3121212);
 	}
 
-	/**
-	 * @see PatientState#getActive(Date)
-	 */
+	
 	@Test
-	public void getActive_shouldReturnTrueIfNotVoidedAndDateInRange() {
-		//given
-		PatientState patientState = new PatientState();
-		patientState.setStartDate(leftRange);
-		patientState.setEndDate(rightRange);
-		patientState.setVoided(false);
-		
-		//when
-		boolean active = patientState.getActive(inRange);
-		
-		//then
-		Assert.assertTrue(active);
+	public void activePatient() {
+		PatientState state = new PatientState();
+		state.setStartDate(start);
+		state.setEndDate(end);
+		state.setVoided(false);
+		//Act
+		boolean isActive = state.getActive(now);
+		//Assert
+		Assert.assertTrue(isActive);
 	}
 
-	/**
-	 * @see PatientState#getActive(Date)
-	 */
+
 	@Test
-	public void getActive_shouldReturnFalseIfVoidedAndDateNotInRange() {
+	public void inActivePatient() {
 		//given
 		PatientState patientState = new PatientState();
-		patientState.setStartDate(leftRange);
-		patientState.setEndDate(rightRange);
+		patientState.setStartDate(start);
+		patientState.setEndDate(end);
 		patientState.setVoided(true);
-		
 		//when
-		boolean active = patientState.getActive(rightOutOfRange);
-		
+		boolean isActive = patientState.getActive(exceeded);
 		//then
-		Assert.assertFalse(active);
+		Assert.assertFalse(isActive);
 	}
 
 	
 	/**
-	 * Test the adding names
+	 * Test adding names
 	 * 
-	 * @throws Exception
 	 */
 	@Test
 	public void addName() {
-		
+		//Arrange
 		Person p = new Person();
-		
-		assertNotNull(p.getNames());
-		
-		PersonName pa1 = new PersonName();
-		
-		pa1.setGivenName("Jon");
-		pa1.setFamilyName("Wick");
-		pa1.setDateCreated(new Date());
-		pa1.setVoided(false);
-		p.addName(pa1);
-		
-		// make sure the name is added.
-		assertTrue("The person object must contain 1 name : " + p.getNames().size(), p
+		PersonName pN = new PersonName();
+		//Act
+		pN.setGivenName("Jon");
+		pN.setFamilyName("Wick");
+		pN.setDateCreated(new Date());
+		pN.setVoided(false);
+		p.addName(pN);
+		//Assert
+		assertTrue("We must have 1 name : " + p.getNames().size(), p
 		        .getNames().size() == 1);
     }
     
 
     /**
-	 * Test the removing of a name
+	 * Test removing names
 	 * 
-	 * @throws Exception
 	 */
 	@Test
 	public void removeName() {
-		
+		//Arrange
 		Person p = new Person();
-		
-		assertNotNull(p.getNames());
-		
-		PersonName pa1 = new PersonName();
-		
-		pa1.setGivenName("firsttest");
-		pa1.setFamilyName("firsttest2");
-		pa1.setDateCreated(new Date());
-		pa1.setVoided(false);
-		p.addName(pa1);
-		
-		// make sure the name is added.
-		assertTrue("The person object must contain 1 name : " + p.getNames().size(), p
-		        .getNames().size() == 1);
-		
+		PersonName pN = new PersonName();
+		//Act
+		pN.setGivenName("Jon");
+		pN.setFamilyName("Wick");
+		pN.setDateCreated(new Date());
+		pN.setVoided(false);
+		p.addName(pN);
+		//Assert
 		// test removing the name
-		p.removeName(pa1);
+		p.removeName(pN);
 		assertTrue("The person object should be empty", p.getNames().size() == 0);
 	}
 	
 
 
-//About the patient's vitals SC 
-
 	/**
-	 * @see Person#addAttribute(PersonAttribute)
+	 * Not saving empty patient's info Test
 	 */
 	@Test
 	public void test_saving_empty_values() {
+		//Arrange
 		Person p = new Person();
-		
-		// make sure there are no initial attributes
-	    //TODO: might delete the first assert method 
-	//	Assert.assertEquals("There should not be any attributes", 0, p.getAttributes().size());
-		
-		PersonAttribute pa1 = new PersonAttribute();
-		pa1.setValue("");
-		pa1.setAttributeType(new PersonAttributeType(1));
-		pa1.setVoided(false);
-		p.addAttribute(pa1);
-		
-		// make sure the attribute was not added
+		PersonAttribute pA = new PersonAttribute();
+		//Act
+		pA.setValue("");
+		pA.setAttributeType(new PersonAttributeType(1));
+		pA.setVoided(false);
+		p.addAttribute(pA);
+		// ensure that nothing was added
+		//Assert
 		Assert.assertEquals("There should not be any attributes", 0, p.getAttributes().size());
 	}
 
 
-	private Person personHelper(boolean isVoid, int attributeType1, int attributeType2, int attributeType3, String attributeName1, String attributeName2, String attributeName3, String attributeValue1, String attributeValue2, String attributeValue3) {
+    // Arrange
+	private Person personConstructor(boolean isVoided, int t1, int t2, int t3, String name1, String name2, String name3, String att1, String att2, String att3) {
 		Person person = new Person();
 
-	 	PersonAttributeType type1 = new PersonAttributeType(attributeType1);
-	 	PersonAttributeType type2 = new PersonAttributeType(attributeType2);
-	 	PersonAttributeType type3 = new PersonAttributeType(attributeType3);
+	 	PersonAttributeType type1 = new PersonAttributeType(t1);
+	 	PersonAttributeType type2 = new PersonAttributeType(t2);
+	 	PersonAttributeType type3 = new PersonAttributeType(t3);
 	    
-	 	type1.setName(attributeName1);
-	 	type2.setName(attributeName2);
-	 	type3.setName(attributeName3);
-	 	PersonAttribute personAttribute1 = new PersonAttribute(type1, attributeValue1);
-	 	PersonAttribute personAttribute2 = new PersonAttribute(type2, attributeValue2);
-	 	PersonAttribute personAttribute3 = new PersonAttribute(type3, attributeValue3);
+	 	type1.setName(name1);
+	 	type2.setName(name2);
+	 	type3.setName(name3);
+	 	PersonAttribute attribute1 = new PersonAttribute(type1, att1);
+	 	PersonAttribute attribute2 = new PersonAttribute(type2, att2);
+	 	PersonAttribute attribute3 = new PersonAttribute(type3, att3);
 	    
-		personAttribute1.setVoided(isVoid);
-		personAttribute2.setVoided(isVoid);
-		personAttribute3.setVoided(isVoid);
+		 attribute1.setVoided(isVoided);
+		 attribute2.setVoided(isVoided);
+		 attribute3.setVoided(isVoided);
 
-		person.addAttribute(personAttribute1);
-		person.addAttribute(personAttribute2);
-		person.addAttribute(personAttribute3);
+		person.addAttribute(attribute1);
+		person.addAttribute(attribute2);
+		person.addAttribute(attribute3);
 
 		return person;
 	}
 
-
-	//This is for finding the patient based on an attribute such as name
-/**
-	 * @see Person#getAttribute(String)
+    /**
+	 * Finding the patient
 	 */
 	@Test
 	public void find_person_based_on_attribute() {
-		Person person = personHelper(false, 1, 2, 3, "name1", "name2", "name3", "value1", "value2", "value3");
-		Assert.assertEquals("name3", person.getAttribute("name3").getAttributeType().getName());
+		//Act
+		Person person = personConstructor(false, 1, 2, 3, "John", "Van", "Wick", "attribute1", "attribute2", "attribute3");
+		//Assert
+		Assert.assertEquals("Wick", person.getAttribute("Wick").getAttributeType().getName());
 	}
 
 
-/**
-	 * @see Person#getAttribute(String)
+     /**
+	 * Not finding a voided patient
 	 */
 	@Test
 	public void cant_find_person_when_voided() {
-		Person person = personHelper(true, 1, 2, 3, "name1", "name2", "name3", "value1", "value2", "value3");
-		Assert.assertNull(person.getAttribute("name3"));
+		//Act
+		Person person = personConstructor(true, 1, 2, 3, "John", "Van", "Wick", "attribute1", "attribute2", "attribute3");
+		//Assert
+		Assert.assertNull(person.getAttribute("Wick"));
 	}
 
 
-
-	//location test
 	/**
-	 * 
-	 * 
-	 * 
+	 * Test the Location
 	 */
 	@Test
 	public void test_location() {
